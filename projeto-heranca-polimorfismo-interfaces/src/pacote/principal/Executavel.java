cd17744ff0ea48e6c04573e2b39592317b86006e
Igor Barros <barrosigor74@gmail.com>
2022-03-13 18:17:13 -0300

    Entendendo Polimorfismo.

--- projeto-heranca-polimorfismo-interfaces/src/pacote/principal/Executavel.java
+++ projeto-heranca-polimorfismo-interfaces/src/pacote/principal/Executavel.java
@@ -9,32 +9,45 @@ public class Executavel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		// impedindo que ela possa ser instânciada.
 		// Pessoa pessoa = new Pessoa();
-		
-		Aluno aluno = new Aluno(); 
+
+		Aluno aluno = new Aluno();
 		aluno.setNome("Igor");
 		aluno.setIdade(19);
-		
+
 		Diretor diretor = new Diretor();
 		diretor.setRegistroGeral("5asd4561d32a13");
 		diretor.setIdade(20);
-		
-		Secretario secretario= new Secretario();
+
+		Secretario secretario = new Secretario();
 		secretario.setExperiencia("Administração");
 		secretario.setIdade(17);
-		
+
 		System.out.println(aluno);
 		System.out.println(diretor);
 		System.out.println(secretario.getExperiencia());
-		
+
 		System.out.println(aluno.pessoaMaiorIdade() + "-" + aluno.msgMaiorIdade());
 		System.out.println(secretario.pessoaMaiorIdade());
 		System.out.println(diretor.pessoaMaiorIdade());
-		
+
 		System.out.println(diretor.salario());
 		System.out.println(secretario.salario());
+
+		/*
+		 * /Polimorfismo / Pessoa pessoa = new Aluno(); pessoa = secretario;
+		 */
+
+		testePolimorfismo(secretario);
+		testePolimorfismo(aluno);
+		testePolimorfismo(diretor);
+	}
+
+	// O método deve ser stático devido a classe main ser static.
+	public static void testePolimorfismo(Pessoa pessoa) {
+		System.out.println("Informações sobre o: " + "nome: " + pessoa.getNome() + "salário de:" + pessoa.salario());
 	}
 
 }
