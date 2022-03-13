76700a047a60b8d42a6302a840571c2de77cf7c8
Igor Barros <barrosigor74@gmail.com>
2022-03-13 11:20:56 -0300

    Gerando a classes Direto e extendendo a classe Pessoa.

---
+++ projeto-heranca-polimorfismo-interfaces/src/pacote/classe/Diretor.java
@@ -0,0 +1,33 @@
+package pacote.classe;
+
+public class Diretor extends Pessoa {
+
+	private String registroEducacao;
+	private String tempoDirecao;
+	private String titulaçao;
+
+	public String getRegistroEducacao() {
+		return registroEducacao;
+	}
+
+	public void setRegistroEducacao(String registroEducacao) {
+		this.registroEducacao = registroEducacao;
+	}
+
+	public String getTempoDirecao() {
+		return tempoDirecao;
+	}
+
+	public void setTempoDirecao(String tempoDirecao) {
+		this.tempoDirecao = tempoDirecao;
+	}
+
+	public String getTitulaçao() {
+		return titulaçao;
+	}
+
+	public void setTitulaçao(String titulaçao) {
+		this.titulaçao = titulaçao;
+	}
+
+}
