6d887c6bc030f63837b62caaf6b6739190f187f9
igor-barros-da-silva <barrosigor74@gmail.com>
2022-03-29 11:01:07 -0300

    Método de impressão de erro na console.

--- projeto-heranca-polimorfismo-interfaces/src/pacote/principal/App.java
+++ projeto-heranca-polimorfismo-interfaces/src/pacote/principal/App.java
@@ -17,60 +17,60 @@ public class App {
 	// Main � um m�todo auto execut�vel em Java.
 	public static void main(String[] args) {
 		
-		// Tratamento do erro.
+	// Tratamento do erro.
 		try {
-
+	
 		// Simples valida��o de permiss�o de acesso
 		String login = JOptionPane.showInputDialog("Informe o login?");
 		String senha = JOptionPane.showInputDialog("Informe o senha?");
-
+	
 		// Bloquear e Autorizar somente quem realmente tem o contrato 100% leg�timo.
 		if (new FuncaoAutorizacao(new Secretario(login, senha)).autorizar()) {
-
+	
 			// Gerando erro
 			List<Aluno> alunos = null;
-
+	
 			// Hashmap - Forma de carregar dados e recuperar por meio de valor.
 			// K = Chave, V = Valor
 			// � uma lista que dentro dela temos uma chave que identifica uma sequ�ni de
 			// valores.
 			HashMap<String, List<Aluno>> maps = new HashMap<String, List<Aluno>>();
-
+	
 			// Iniciando os valores.
 			maps.putIfAbsent(StatusAluno.APROVADO, new ArrayList<Aluno>());
 			maps.putIfAbsent(StatusAluno.RECUPERACAO, new ArrayList<Aluno>());
 			maps.putIfAbsent(StatusAluno.REPROVADO, new ArrayList<Aluno>());
-
+	
 			for (int quantidade = 1; quantidade <= 3; quantidade++) {
-
+	
 				// Entrada de dados com o teclado.
 				String nome = JOptionPane.showInputDialog("Qual o nome do aluno " + quantidade + "?");
 				String idade = JOptionPane.showInputDialog("Qual a idade do aluno?");
-
+	
 				// Objeto est� na mem�ria, new = cria um novo objeto.
 				// M�todo construtor.
 				Aluno aluno = new Aluno();
-
+	
 				// Nome din�mico atrav�s da entrada de dados com teclado.
 				// Convertendo integer para string.
 				aluno.setNome(nome);
 				aluno.setIdade(Integer.valueOf(idade));
-
+	
 				// Criando a lista din�mica das disciplinas do aluno.
 				for (int posicao = 1; posicao <= 1; posicao++) {
 					String nomeDisciplina = JOptionPane.showInputDialog("Qual o nome da disciplina " + posicao + "?");
 					String notaDisciplina = JOptionPane.showInputDialog("Qual a nota da disciplina " + posicao + "?");
-
+	
 					Disciplina disciplina = new Disciplina();
 					disciplina.setDisciplina(nomeDisciplina);
 					disciplina.setNota(Double.valueOf(notaDisciplina));
-
+	
 					aluno.getDisciplinas().add(disciplina);
 				}
-
+	
 				// Removendo disciplinas da lista do aluno.
 				int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
-
+	
 				if (escolha == 0) {
 					// Op��o Sim � 0
 					int continuarRemover = 0;
@@ -87,32 +87,32 @@ public class App {
 				// Variável nula.
 				alunos.add(aluno);
 			}
-
+	
 			// Separando em listas.
 			for (Aluno aluno : alunos) {
-
+	
 				if (aluno.getAlunoAprovado1().equalsIgnoreCase(StatusAluno.APROVADO)) {
 					maps.get(StatusAluno.APROVADO).add(aluno);
 				} else if (aluno.getAlunoAprovado1().equalsIgnoreCase(StatusAluno.RECUPERACAO)) {
 					maps.get(StatusAluno.RECUPERACAO).add(aluno);
-
+	
 				} else if (aluno.getAlunoAprovado1().equalsIgnoreCase(StatusAluno.REPROVADO)) {
 					maps.get(StatusAluno.REPROVADO).add(aluno);
 				}
 			}
-
+	
 			System.out.println("-----------------------------------------------");
-
+	
 			for (Aluno aluno : maps.get(StatusAluno.APROVADO)) {
 				System.out.println("Nome: " + aluno.getNome() + "Lista de alunos aprovados: "
 						+ aluno.getAlunoAprovado1() + "M�dia: " + aluno.getMediaNota());
 			}
-
+	
 			for (Aluno aluno : maps.get(StatusAluno.RECUPERACAO)) {
 				System.out.println("Nome: " + aluno.getNome() + "Lista de alunos em recupera��o: "
 						+ aluno.getAlunoAprovado1() + "M�dia: " + aluno.getMediaNota());
 			}
-
+	
 			for (Aluno aluno : maps.get(StatusAluno.REPROVADO)) {
 				System.out.println("Nome: " + aluno.getNome() + "Lista de alunos reprovados: "
 						+ aluno.getAlunoAprovado1() + "M�dia: " + aluno.getMediaNota());
@@ -122,8 +122,18 @@ public class App {
 		}
 	}
 		catch (Exception e) {
+			// É OBRIGATÓRIO DEBUGAR E ENCONTRAR O ERRO
+			// Imprime erro na console.
 			e.printStackTrace();
-			JOptionPane.showMessageDialog(null, "Erro ao processar notas!");
+			// Mensagem do erro.
+			JOptionPane.showMessageDialog(null, "Error:" + e.getMessage());			
+			
+			// Toda informações de erro em Array.
+			for(int i = 0; i < e.getStackTrace().length; i++) {
+				System.out.println("Erro de classe:" + e.getStackTrace()[i].getClassName());
+				System.out.println("Erro de método:" + e.getStackTrace()[i].getMethodName());
+				System.out.println("Erro de linha1:" + e.getStackTrace()[i].getLineNumber());
+			}
 		}
 	}
 }
