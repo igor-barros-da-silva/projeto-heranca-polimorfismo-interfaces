b644cf25c8e5e170a4b0f7797ed29dde577db56f
igor-barros-da-silva <barrosigor74@gmail.com>
2022-03-30 13:24:58 -0300

    Criando array de notas.

---
+++ projeto-heranca-polimorfismo-interfaces/src/pacote/arrayVetor/ArrayVetor.java
@@ -0,0 +1,18 @@
+package pacote.arrayVetor;
+
+public class ArrayVetor {
+
+	public static void main(String[] args) {
+		
+		// Array sempre deve ter a quantidade de posições definida.
+		double[] notas = new double[4];
+		// Atribuir valor na posição do array.
+		notas[0] = 9.8;
+		notas[1] = 7.3;
+		notas[2] = 9.3;
+		notas[3] = 7.8;
+		
+		System.out.println(notas);
+	}
+
+}
