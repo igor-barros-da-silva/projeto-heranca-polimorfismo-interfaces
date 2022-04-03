0bbd315f8f65dde15c8490198c3a478f4ea8fbd5
igor-barros-da-silva <barrosigor74@gmail.com>
2022-04-02 21:42:06 -0300

    Criando uma Matriz multidimensional.

---
+++ projeto-heranca-polimorfismo-interfaces/src/pacote/principal/Matriz.java
@@ -0,0 +1,21 @@
+package pacote.principal;
+
+public class Matriz {
+
+	public static void main(String[] args) {
+		
+		int notas[][] = new int[2][3];
+		
+		notas[0][0] = 80;
+		notas[0][1] = 90;
+		notas[0][2] = 60;
+		
+		notas[1][0] = 55;
+		notas[1][1] = 70;
+		notas[1][2] = 87;
+		
+		System.out.println(notas);
+
+	}
+
+}
