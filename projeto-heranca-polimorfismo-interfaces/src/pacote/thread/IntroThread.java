ed8ee38d6a0de392b471c01c78929b083ba3b37a
igor-barros-da-silva <barrosigor74@gmail.com>
2022-04-10 03:14:41 -0300

    Thread em objeto e o Runnable.

--- projeto-heranca-polimorfismo-interfaces/src/pacote/thread/IntroThread.java
+++ projeto-heranca-polimorfismo-interfaces/src/pacote/thread/IntroThread.java
@@ -7,67 +7,65 @@ public class IntroThread {
 	public static void main(String[] args) throws InterruptedException {
 
 		/* Thread 1 processando em paralelo do envio de email. */
-		new Thread() {
-			/*
-			 * Método run onde ficam os procedimentos que você deseja executar
-			 * paralelamente.
-			 */
-			public void run() {
-				// Código da rotina.
-				// Início do código da rotina executado em paralelo.
-				for (int pos = 0; pos < 10; pos++) {
-
-					// Execução desse envio com um tempo de parada, ou com tempo determinado.
-					System.out.println("Executando alguma rotina, por exemplo envio de e-mail");
-
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-
-						e.printStackTrace();
-					} // Faz com que o thread atual suspenda a execução por um período especificado.
-				} // Fim do código da rotina executado em paralelo.
-			};
-
-		}.start(); /*
-					 * Iniciando o processamento paralelo, e liberando o programa para executar
-					 * qualquer outra thread.
-					 */
+		/* Transformando a Thread em objeto manipulável. */
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
 		
 		// DIVISÃO DAS THREAD
 		
 		/* Thread 2 processando em paralelo do envio de nota fiscal. */
-		new Thread() {
-			/*
-			 * Método run onde ficam os procedimentos que você deseja executar
-			 * paralelamente.
-			 */
-			public void run() {
-				// Código da rotina.
-				// Início do código da rotina executado em paralelo.
-				for (int pos = 0; pos < 10; pos++) {
-
-					// Execução desse envio com um tempo de parada, ou com tempo determinado.
-					System.out.println("Executando alguma rotina, por exemplo envio de nota fiscal");
-
-					try {
-						Thread.sleep(2000);
-					} catch (InterruptedException e) {
-
-						e.printStackTrace();
-					} // Faz com que o thread atual suspenda a execução por um período especificado.
-				} // Fim do código da rotina executado em paralelo.
-			};
-
-		}.start(); /*
-					 * Iniciando o processamento paralelo, e liberando o programa para executar
-					 * qualquer outra thread.
-					 */
+		/* Transformando a Thread em objeto manipulável. */
+		Thread threadNFCE = new Thread(thread2);
+		threadNFCE.start();
 
 		// Código do sistema do usuário continua o fluxo de trabalho
 		System.out.println("FIM DO CÓDIGO DE TESTE DE THREAD!");
 		// Fluxo do sistema, por exemplo: cadastro de venda, emissão de nota fiscal.
 		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário!");
 	}
+	
+	private static Runnable thread1 = new Runnable() {
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			// Código da rotina.
+			// Início do código da rotina executado em paralelo.
+			for (int pos = 0; pos < 10; pos++) {
+
+				// Execução desse envio com um tempo de parada, ou com tempo determinado.
+				System.out.println("Executando alguma rotina, por exemplo envio de e-mail");
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+
+					e.printStackTrace();
+				} // Faz com que o thread atual suspenda a execução por um período especificado.
+			} // Fim do código da rotina executado em paralelo.
+		}
+	};
+	
+	private static Runnable thread2 = new Runnable() {
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			// Código da rotina.
+			// Início do código da rotina executado em paralelo.
+			for (int pos = 0; pos < 10; pos++) {
+
+				// Execução desse envio com um tempo de parada, ou com tempo determinado.
+				System.out.println("Executando alguma rotina, por exemplo envio de nota fiscal");
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+
+					e.printStackTrace();
+				} // Faz com que o thread atual suspenda a execução por um período especificado.
+			} // Fim do código da rotina executado em paralelo.
+		}
+	};
 
 }
